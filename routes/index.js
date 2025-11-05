f5c609fc4584c14cfc879460f3c3c30d20e8d337
Andr203i <andreas26gvs@gmail.com>
2025-11-05 17:54:17 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(`
+    <h1>🏗️ Svar fra Eksamen-Dis-2025</h1>
+    <p>Dette er Node/Express-serveren i <b>Frankfurt</b> (port 3000)</p>
+    <p>Klientens IP: ${req.ip}</p>
+  `);
 });
 
 module.exports = router;
