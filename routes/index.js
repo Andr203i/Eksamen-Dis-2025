e33dd7c9db63bc1a0a8069e5f55aab654fcbbd80
Andr203i <andreas26gvs@gmail.com>
2025-11-13 13:36:28 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,30 @@
 var express = require('express');
 var router = express.Router();
-const pool = require('../db');
+const pool = require('../db'); // Importér database-poolen
 
-/* GET home page. */
+/**
+ * GET /
+ * Forside route – tester databaseforbindelsen
+ */
 router.get('/', async function(req, res, next) {
   try {
+    // Simpel DB-testquery
     const [rows] = await pool.query('SELECT 1 + 1 AS result');
+
+    // Send svar til browseren
     res.send('DB virker! 1 + 1 = ' + rows[0].result);
+
   } catch (err) {
     console.error('Fejl ved DB-forbindelse:', err);
     res.status(500).send('Fejl ved forbindelse til databasen');
   }
 });
 
-module.exports = router;
+/**
+ * Eksempel på en ekstra route hvis du får brug for det senere:
+ * router.get('/status', (req, res) => {
+ *   res.json({ message: 'Serveren kører!', time: new Date() });
+ * });
+ */
 
+module.exports = router;
